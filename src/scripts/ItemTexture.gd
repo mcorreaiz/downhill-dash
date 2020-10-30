0a9b07b31c42eddcab49e171fdc11df3bffcb1a1
Matias <mjcorrea5@uc.cl>
2020-10-30 18:34:36 -0300

    Implement instantiating new obstacles

---
+++ src/scripts/ItemTexture.gd
@@ -0,0 +1,14 @@
+extends TextureRect
+
+export(PackedScene) var this_scene
+onready var object_cursor = get_node("/root/Editor/EditorObject")
+onready var cursor_sprite = object_cursor.get_node("Sprite")
+
+func _ready():
+	connect("gui_input", self, "_item_clicked")
+
+func _item_clicked(event):
+	if event is InputEvent:
+		if event.is_action_pressed("mb_left"):
+			object_cursor.current_item = this_scene
+			cursor_sprite.texture = texture
