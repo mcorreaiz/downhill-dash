7204c383d8ff3ef4f49516500b00914ddc24fe30
Matias <mjcorrea5@uc.cl>
2020-10-31 13:22:53 -0300

    Drag and Drop mechanics for level creation

--- src/scripts/EditorObject.gd
+++ src/scripts/EditorObject.gd
@@ -1,24 +1,21 @@
 extends Node2D
 
 var can_place = true
-onready var level = get_node("/root/Editor/Level")
+onready var level = get_node("../Level")
+onready var item_select = get_node("../ItemSelect")
 var current_item
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	global_position = get_global_mouse_position()
 	
-	if (current_item != null and can_place and Input.is_action_just_pressed("mb_left")):
-		var new_item = current_item.instance()
-		level.add_child(new_item)
-		new_item.global_position = get_global_mouse_position()
+	can_place = !item_select.get_rect().has_point(global_position)
+	
+	if Input.is_action_just_released("mb_left"):
+		if current_item != null and can_place:
+			var new_item = current_item.instance()
+			level.add_child(new_item)
+			new_item.global_position = get_global_mouse_position()
+			
+		current_item = null
+		$Sprite.texture = null
