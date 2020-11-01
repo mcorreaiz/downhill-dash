0fc781d53953144efcfaa7533af650ffd5d2fe94
Matias <mjcorrea5@uc.cl>
2020-11-01 19:52:50 -0300

    Drag and Drop to move existing objects

--- src/scripts/EditorObject.gd
+++ src/scripts/EditorObject.gd
@@ -5,6 +5,8 @@ onready var level = get_node("../Level")
 onready var item_select = get_node("../ItemSelect")
 var current_item
 
+func _ready():
+	Globals.mode = Globals.EDIT
 
 func _process(delta):
 	global_position = get_global_mouse_position()
@@ -13,7 +15,7 @@ func _process(delta):
 	
 	if Input.is_action_just_released("mb_left"):
 		if current_item != null and can_place:
-			var new_item = current_item.instance()
+			var new_item = current_item
 			level.add_child(new_item)
 			new_item.global_position = get_global_mouse_position()
 			
--- src/scripts/Globals.gd
+++ src/scripts/Globals.gd
@@ -3,3 +3,18 @@
 extends Node
 
 const FIREBASE_API_KEY := ""
+enum {
+	PLAY
+	EDIT
+}
+
+var mode = PLAY
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
